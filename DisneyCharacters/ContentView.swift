a6917b8c350d3cd5fa9c8ff10a0830a45821f2cf
Clearcazter <azpitarte14_@hotmail.com>
2026-06-13 09:23:40 -0600

    Initial Commit

---
+++ DisneyCharacters/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DisneyCharacters
+//
+//  Created by Alejandro Azpitarte on 13/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DisneyCharacters/DisneyCharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyCharactersApp.swift
+//  DisneyCharacters
+//
+//  Created by Alejandro Azpitarte on 13/06/26.
+//
+
+import SwiftUI
+
+@main
+struct DisneyCharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
